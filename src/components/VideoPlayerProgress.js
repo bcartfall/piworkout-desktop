86e9c0d0ea6bb25e75ee1406a3d153b7095fc2d4
bcartfall <bcartfall@yahoo.com>
2023-03-03 17:01:28 -0800

    Remove debug.

--- src/components/VideoPlayerProgress.js
+++ src/components/VideoPlayerProgress.js
@@ -64,7 +64,6 @@ export default function VideoPlayerProgress({ currentVideo, progress, onChangePr
   };
 
   const onMouseEnter = useCallback(() => {
-    const found = !!(storyBoardImage.current && bif.current && bif.current[currentVideo.id]);
     if (storyBoardImage.current && bif.current && bif.current[currentVideo.id]) {
       storyBoardImage.current.style.display = 'block';
     }
